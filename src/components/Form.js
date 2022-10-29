f9606ea7ca18868e4755779c4cc578f079785eb9
how1219 <99299186+how1219@users.noreply.github.com>
2022-10-28 23:17:19 -0700

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,22 +2,6 @@ import React from "react"
 import { useForm } from "react-hook-form";
 
 export default function Form() {
-    // const [indexes, setIndexes] = React.useState([]);
-    // const [counter, setCounter] = React.useState(0);
-    // const { register, handleSubmit } = useForm();
-
-    // const addFriend = () => {
-    //     setIndexes(prevIndexes => [...prevIndexes, counter]);
-    //     setCounter(prevCounter => prevCounter + 1);
-    //   };
-    
-    // const removeFriend = index => () => {
-    //     setIndexes(prevIndexes => [...prevIndexes.filter(item => item !== index)]);
-    //     setCounter(prevCounter => prevCounter - 1);
-    //   };
-    // const clearFriends = () => {
-    //     setIndexes([]);
-    //   };
     
 
     return (
@@ -33,42 +17,6 @@ export default function Form() {
       </fieldset>
 
     
-      {/* {indexes.map(index => {
-        const fieldName = `friends[${index}]`;
-        return (
-          <fieldset name={fieldName} key={fieldName}>
-            <label>
-              First Name {index}:
-              <input
-                type="text"
-                name={`${fieldName}.firstName`}
-                ref={register}
-              />
-            </label>
-
-            <label>
-              Last Name {index}:
-              <input
-                type="text"
-                name={`${fieldName}.lastName`}
-                ref={register}
-              />
-            </label>
-            <button type="button" onClick={removeFriend(index)}>
-              Remove
-            </button>
-          </fieldset>
-        );
-      })}
-
-
-      <button type="button" onClick={addFriend}>
-        Add Friend
-      </button>
-      <button type="button" onClick={clearFriends}>
-        Clear Friends
-      </button>
-       */}
 
        <fieldset>
         <h4>Meat</h4>
