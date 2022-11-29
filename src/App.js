0106f0b04813387d38322800f832a650c7740d8a
how10 <how10@students.cs.ubc.ca>
2022-11-28 22:56:02 -0800

    added button effects

--- src/App.js
+++ src/App.js
@@ -6,79 +6,79 @@ import VeggieCard from "./components/VeggieCard";
 import veggieData from "./dataVeggie"
 import RiceNoodleCard from "./components/RiceNoodleCard";
 import riceNoodleData from "./dataRiceNoodle"
-import {useState} from 'react'
+import { useState } from 'react'
 import { useRef } from 'react'
 import Form from "./components/Form";
 
 function App() {
   const meatCards = meatData.map(item => {
     return (
-        <MeatCard
-            key={item.id}
-            item={item}
-        />
+      <MeatCard
+        key={item.id}
+        item={item}
+      />
     )
-})
+  })
 
-const veggieCards = veggieData.map(item => {
-  return (
+  const veggieCards = veggieData.map(item => {
+    return (
       <VeggieCard
-          key={item.id}
-          item={item}
+        key={item.id}
+        item={item}
       />
-  )
-})
+    )
+  })
 
-const riceNoodleCards = riceNoodleData.map(item => {
-  return (
+  const riceNoodleCards = riceNoodleData.map(item => {
+    return (
       <RiceNoodleCard
-          key={item.id}
-          item={item}
+        key={item.id}
+        item={item}
       />
-  )
-})
+    )
+  })
 
-const [isShown, setIsShown] = useState(false)
-const ref = useRef(null);
-const handleClick = event => {
-  setIsShown(true);
-  ref.current?.scrollIntoView({behavior: 'smooth'});
-}
+  const [isShown, setIsShown] = useState(false)
+  const ref = useRef(null);
+  const handleClick = event => {
+    setIsShown(true);
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  }
 
 
 
   return (
     <div>
-        <Navbar />
-        <div className="top">
-        <img src="../meatImages/topImage.png" alt="Top" className="topImage"/>
-        </div>
-        <h2 className="meat--title">Meat Mains</h2>
-        <section className="cards-list">
-                {meatCards}
-        </section>
+      <Navbar />
+      <div className="top">
+        <img src="../meatImages/topImage.png" alt="Top" className="topImage" />
+      </div>
+      <h2 className="meat--title">Meat Mains</h2>
+      <section className="cards-list">
+        {meatCards}
+      </section>
 
-        <h2 className="veggie--title">Veggie</h2>
-        <section className="cards-list">
-                {veggieCards}
-        </section>
+      <h2 className="veggie--title">Veggie</h2>
+      <section className="cards-list">
+        {veggieCards}
+      </section>
 
-        <h2 className="rice--title">Rice or Noodle Mains</h2>
-        <section className="cards-list">
-                {riceNoodleCards}
-        </section>
+      <h2 className="rice--title">Rice or Noodle Mains</h2>
+      <section className="cards-list">
+        {riceNoodleCards}
+      </section>
 
-        <div className="order">
-          <button className="order-button" onClick={handleClick}>Order Now!</button>
-          <div style={{height: 20}} />
-          <div ref={ref}>{isShown && <Form />}</div>
-          <div style={{height: 20}} />
-          
-        </div>
+      <div className="order">
+        <button className="order-button" onClick={handleClick}>Order Now!</button>
+        <div style={{ height: 20 }} />
+        <div ref={ref}>{isShown && <Form />}</div>
+        <div style={{ height: 20 }} />
 
-      
+      </div>
+
+
     </div>
-    )   
+  )
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,95 +1,95 @@
 import React from "react"
 
 export default function Form() {
-    return (
-        <div>
-        
-         <h1 className="form--title">Food Order Form</h1>
-            <p className="form--note">Please fill out this form to order food</p>
-        <form action="https://getform.io/f/2918b7f1-d47a-401a-bf6d-f72da9ff4e3b" method = "POST">
-      <fieldset>
-        <label for="first-name">Enter Your First Name: <input id="first-name" name="first-name" type="text" required /></label>
-        <label for="last-name">Enter Your Last Name: <input id="last-name" name="last-name" type="text" required /></label>
-        <label for="tel">Enter Your Phone Number: <input id="phone" name="phone" type="tel" pattern="[0-9]{10}" /></label>
-      </fieldset>
+  return (
+    <div>
+
+      <h1 className="form--title">Food Order Form</h1>
+      <p className="form--note">Please fill out this form to order food</p>
+      <form action="https://getform.io/f/2918b7f1-d47a-401a-bf6d-f72da9ff4e3b" method="POST">
         <fieldset>
-        <h4>Meat</h4>
-        <label className="items">
-            <input  id="Thai Mango Chicken" type="number" min="0" name="Thai Mango Chicken" className="inline" placeholder="quantity" />Thai Mango Chicken
-        </label>
-        <label for="options" name="options" className="items">
-          <input id="Santa Barbara Chicken" type="number" min="0" name="Santa Barbara Chicken" className="inline"/>Santa Barbara Chicken
-        </label>
-        <label className="items">
-          <input id="General Thai Chicken" type="number" min="0" name="General Thai Chicken" className="inline" />General Thai Chicken
-        </label>
-        <label className="items">
-          <input id="Bangkok Chicken" type="number" min="0" name="Bangkok Chicken" className="inline" />Bangkok Chicken
-        </label>
-        <label className="items">
-          <input id="Red Curry Chicken" type="number" min="0" name="Red Curry Chicken" className="inline" />Red Curry Chicken
-        </label>      
-        <label className="items">
-          <input id="Green Curry Chicken" type="number" min="0" name="Green Curry Chicken" className="inline" />Green Curry Chicken
-        </label>
-        <label className="items">
-          <input id="Thai Chill Fish" type="number" min="0" name="Thai Chill Fish" className="inline" />Thai Chill Fish
-        </label>
-        <h4>Veggie</h4>
-        <label className="items">
-          <input id="Sweet Potato" type="number" min="0" name="Sweet Potato" className="inline" placeholder="quantity"/>Sweet Potato
-        </label>
-        <label className="items">
-          <input id="Eggplant Tofu" type="number" min="0" name="Eggplant Tofu" className="inline" />Eggplant Tofu
-        </label>
-        <label className="items">
-          <input id="Chill Green Bean" type="number" min="0" name="Chill Green Bean" className="inline" />Chill Green Bean
-        </label>
-        <label className="items">
-          <input id="Veggie Mix" type="number" min="0" name="Veggie Mix" className="inline" />Veggie Mix
-        </label>
-        <label className="items">
-          <input id="Spring Roll" type="number" min="0" name="Spring Roll" className="inline" />Spring Roll
-        </label>
-
-        <h4>Rice or Noddle Mains</h4>
-        <label className="items">
-          <input id="Thai Basil Fried Rice" type="number" min="0" name="Thai Basil Fried Rice" className="inline" placeholder="quantity"/>Thai Basil Fried Rice
-        </label>
-         <label className="items">
-          <input id="Pineapple Fried Rice" type="number" min="0" name="Pineapple Fried Rice" className="inline" />Pineapple Fried Rice
-         </label>
-         <label className="items">
-           <input id="Classic Pad Thai" type="number" min="0" name="Classic Pad Thai" className="inline" />Classic Pad Thai
-         </label>
-         <label className="items">
-           <input id="Classic Pad Thai Spicy" type="number" min="0" name="Classic Pad Thai Spicy" className="inline" />Classic Pad Thai Spicy</label>
-         <label className="items">
-          <input id="White Rice" type="number" min="0" name="White Rice" className="inline" />White Rice</label>
-         <h4>Drinks</h4>
-         <label className="items">
-           <input id="Apple Juice" type="number" min="0" name="Apple Juice" className="inline" placeholder="quantity"/>Apple Juice</label>
-         <label className="items">
-           <input id="Orange Juice" type="number" min="0" name="Orange Juice" className="inline" />Orange Juice</label>
-         <label className="items">
-           <input id="Iced Tea" type="number" min="0" name="Iced Tea" className="inline" />Iced Tea</label>
-         <label className="items">
-           <input id="Pop" type="number" min="0" name="Pop" className="inline" />Pop</label>
-       </fieldset>
-
-
-      <fieldset>
-        <label for="note">Note:
-          <textarea id="note" name="note" rows="3" cols="30" placeholder="eg: Nooo spicy"></textarea>
-        </label>
-        <label for="utensils" name="utensils">
-          <input id="utensils" type="checkbox" name="utensils" className="inline" />Include utensils and condiments
-        </label>
-        <label for="pick-up-time">Your order will be ready in <strong>30 minutes</strong>.
+          <label for="first-name">Enter Your First Name: <input id="first-name" name="first-name" type="text" required /></label>
+          <label for="last-name">Enter Your Last Name: <input id="last-name" name="last-name" type="text" required /></label>
+          <label for="tel">Enter Your Phone Number: <input id="phone" name="phone" type="tel" pattern="[0-9]{10}" /></label>
+        </fieldset>
+        <fieldset>
+          <h4>Meat</h4>
+          <label className="items">
+            <input id="Thai Mango Chicken" type="number" min="0" name="Thai Mango Chicken" className="inline" placeholder="quantity" />Thai Mango Chicken
           </label>
-      </fieldset>
-      <input type="submit" value="Submit" />
-     </form>
+          <label for="options" name="options" className="items">
+            <input id="Santa Barbara Chicken" type="number" min="0" name="Santa Barbara Chicken" className="inline" />Santa Barbara Chicken
+          </label>
+          <label className="items">
+            <input id="General Thai Chicken" type="number" min="0" name="General Thai Chicken" className="inline" />General Thai Chicken
+          </label>
+          <label className="items">
+            <input id="Bangkok Chicken" type="number" min="0" name="Bangkok Chicken" className="inline" />Bangkok Chicken
+          </label>
+          <label className="items">
+            <input id="Red Curry Chicken" type="number" min="0" name="Red Curry Chicken" className="inline" />Red Curry Chicken
+          </label>
+          <label className="items">
+            <input id="Green Curry Chicken" type="number" min="0" name="Green Curry Chicken" className="inline" />Green Curry Chicken
+          </label>
+          <label className="items">
+            <input id="Thai Chill Fish" type="number" min="0" name="Thai Chill Fish" className="inline" />Thai Chill Fish
+          </label>
+          <h4>Veggie</h4>
+          <label className="items">
+            <input id="Sweet Potato" type="number" min="0" name="Sweet Potato" className="inline" placeholder="quantity" />Sweet Potato
+          </label>
+          <label className="items">
+            <input id="Eggplant Tofu" type="number" min="0" name="Eggplant Tofu" className="inline" />Eggplant Tofu
+          </label>
+          <label className="items">
+            <input id="Chill Green Bean" type="number" min="0" name="Chill Green Bean" className="inline" />Chill Green Bean
+          </label>
+          <label className="items">
+            <input id="Veggie Mix" type="number" min="0" name="Veggie Mix" className="inline" />Veggie Mix
+          </label>
+          <label className="items">
+            <input id="Spring Roll" type="number" min="0" name="Spring Roll" className="inline" />Spring Roll
+          </label>
+
+          <h4>Rice or Noddle Mains</h4>
+          <label className="items">
+            <input id="Thai Basil Fried Rice" type="number" min="0" name="Thai Basil Fried Rice" className="inline" placeholder="quantity" />Thai Basil Fried Rice
+          </label>
+          <label className="items">
+            <input id="Pineapple Fried Rice" type="number" min="0" name="Pineapple Fried Rice" className="inline" />Pineapple Fried Rice
+          </label>
+          <label className="items">
+            <input id="Classic Pad Thai" type="number" min="0" name="Classic Pad Thai" className="inline" />Classic Pad Thai
+          </label>
+          <label className="items">
+            <input id="Classic Pad Thai Spicy" type="number" min="0" name="Classic Pad Thai Spicy" className="inline" />Classic Pad Thai Spicy</label>
+          <label className="items">
+            <input id="White Rice" type="number" min="0" name="White Rice" className="inline" />White Rice</label>
+          <h4>Drinks</h4>
+          <label className="items">
+            <input id="Apple Juice" type="number" min="0" name="Apple Juice" className="inline" placeholder="quantity" />Apple Juice</label>
+          <label className="items">
+            <input id="Orange Juice" type="number" min="0" name="Orange Juice" className="inline" />Orange Juice</label>
+          <label className="items">
+            <input id="Iced Tea" type="number" min="0" name="Iced Tea" className="inline" />Iced Tea</label>
+          <label className="items">
+            <input id="Pop" type="number" min="0" name="Pop" className="inline" />Pop</label>
+        </fieldset>
+
+
+        <fieldset>
+          <label for="note">Note:
+            <textarea id="note" name="note" rows="3" cols="30" placeholder="eg: Nooo spicy"></textarea>
+          </label>
+          <label for="utensils" name="utensils">
+            <input id="utensils" type="checkbox" name="utensils" className="inline" />Include utensils and condiments
+          </label>
+          <label for="pick-up-time">Your order will be ready in <strong>30 minutes</strong>.
+          </label>
+        </fieldset>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
-    )
+  )
 }
